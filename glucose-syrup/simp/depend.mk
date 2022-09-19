27252cb0bd61162753114527b054fc8904d5339b
giuseppe mazzotta <giuseppemazzotta8@gmail.com>
2022-09-19 18:24:18 +0200

    adding glucose

---
+++ glucose-syrup/simp/depend.mk
@@ -0,0 +1,13 @@
+/home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/SimpSolver.o /home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/SimpSolver.or /home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/SimpSolver.od /home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/SimpSolver.op: \
+ /home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/SimpSolver.cc \
+ ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
+ ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h \
+ ../mtl/Alg.h ../utils/Options.h ../utils/ParseUtils.h \
+ ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h ../core/BoundedQueue.h \
+ ../core/Constants.h ../mtl/Clone.h ../utils/System.h
+/home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/Main.o /home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/Main.or /home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/Main.od /home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/Main.op: /home/giuseppe/HerbrandBaseGenerator/glucose-syrup/simp/Main.cc \
+ ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
+ ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
+ ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
+ ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h \
+ ../core/BoundedQueue.h ../core/Constants.h ../mtl/Clone.h
